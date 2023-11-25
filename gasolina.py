580f9ca8c915c08a9c57f8760b6350fd57f9cb71
EduardoFontouraDias <eduardofontour@gmail.com>
2023-11-25 17:46:09 +0000

    Atualização do código para plotagem do gráfico

--- gasolina.py
+++ gasolina.py
@@ -1,20 +1,15 @@
+# código de geração do gráfico
 import pandas as pd,seaborn as sns, matplotlib.pyplot as plt
 
-
 #Abertura de arquivo CSV para extração dos dados
-with open(file='./gasolina.csv',mode='r',encoding='utf8') as arquivo:
-  dados = pd.read_csv(arquivo)
+with open(file='gasolina.csv',mode='r',encoding='utf8') as arquivo:
+  dados_df = pd.read_csv(arquivo)
 
-#Transformação dos dados em CSV para DataFrame do Pandas para manipulação
-gasolina_df = pd.DataFrame(dados)
-print(len(gasolina_df))
-gasolina_df.head()
+#Plotagem do gráfico de linha com os dados do dados
+with sns.axes_style('whitegrid'):
+  grafico_gasolina = sns.lineplot(data=dados_df,x='dia',y='venda',color='green')
+  grafico_gasolina.set(title='Preço de venda por dia em julho de 2021',ylabel = 'Preço de Venda(R$)',xlabel='Dias')
+  grafico_gasolina.figure.set_size_inches(w=25/2.54,h=15/2.54)
 
-#Plotagem do gráfico de linha com os dados do gasolina_df
-with sns.axes_style('darkgrid'):
-  grafico_gasolina = sns.lineplot(data=gasolina_df,x='dia',y='venda',color = 'red')
-  grafico_gasolina.set(title='Gráfico de variação do preço da gasolina',xlabel = 'Dias',ylabel = 'Preço(R$)')
-  grafico_gasolina.figure.set_size_inches(w=25/2.54,h=10/2.54)
-
-  #Salvar o arquivo como imagem no formato .png
+  #Plotagem do gráfico de linha com os dados do dados_df
   plt.savefig('gasolina.png')
